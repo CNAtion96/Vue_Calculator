5f7d5d524a206a7bcdf721dfcb6efefa80b0357d
Chris Adkins <chris_adkins@Chriss-MacBook-Pro.local>
2019-02-07 13:04:30 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calculator />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Calculator from './components/Calculator.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    Calculator,
   },
 };
 </script>
 
 <style lang="scss">
+body {
+  padding: 0px;
+  margin: 0px;
+  overflow: none;
+  * {
+    box-sizing: border-box;
+  }
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  width: 100vw;
+  margin: 0px;
+  padding: 0px;
+  background-color: lightblue;
 }
 </style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="calculator">
+    <div id="display"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .calculator {
+    position: absolute;
+    width: 300px;
+    height: 500px;
+    border: 1px solid black;
+    background-color: #efefef;
+    top: 50%;
+    left: 50%;
+    margin: -250px 0 0 -150px;
+
+    #display {
+      width: 100%;
+      height: 42%;
+      border-bottom: 1px solid gray;
+    }
+  }
+</style>
